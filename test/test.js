3d50ff2ec0fa52e4e13af23ed25707d20d2117be
Jimin Park <pjm950@gmail.com>
2013-01-05 15:28:26 +0900

    Added the part reading CHARSHAPE and DOCSUMMARY.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var HWPDoc = require('../').HWPDocument;
-var openThis1 = new HWPDoc('openThis1.hwp');
+var openThis1 = new HWPDoc('Style.hwp');
 openThis1.on('ready', function(){
 	console.log(openThis1.toHMLString());
 });
