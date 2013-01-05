f51ddc9ae9a74b72cd3c0809ed1fbd2fb160c536
Jimin Park <pjm950@gmail.com>
2013-01-06 05:07:48 +0900

    Added PARASHAPE and STYLE.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 var HWPDoc = require('../').HWPDocument;
 var openThis1 = new HWPDoc('Style.hwp');
+console.time('HWP -> HML');
 openThis1.on('ready', function(){
 	console.log(openThis1.toHMLString());
+	console.timeEnd('HWP -> HML');
 });
